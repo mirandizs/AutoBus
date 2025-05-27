df8e768d7cbed89bedf78818f08847c3d42c2822
Sofia Miranda <149806274+mirandizs@users.noreply.github.com>
2025-05-27 16:46:45 +0100

    instalacao de bibliotecas e criacao do gerador de recibos

--- src/app/Paginas/Compras/compras.ts
+++ src/app/Paginas/Compras/compras.ts
@@ -4,10 +4,11 @@ import { Footer } from '../../Componentes/Footer/footer';
 import { ServicoAutenticacao } from '../../Services/Autenticacao.service';
 import { HttpService } from '../../Services/Http.service';
 import { Definicoes } from '../../Definicoes';
+import { saveAs } from 'file-saver'; // caso ainda não tenhas
 
 @Component({
     selector: 'compras',
-    imports: [Topbar, Footer],
+    imports: [Topbar],
     templateUrl: './compras.html',
     styleUrl: './compras.css'
   })
@@ -24,6 +25,12 @@ export class PaginaCompras {
         this.Compras = await this.ServicoHTTP.Request(Pedido_URL, "GET")
     }
 
-    // transferirRecibo(id: number) {
+
+    async transferirRecibo(idCompra: number) { //LocalPartida: string
+        const Pedido_URL = new URL(Definicoes.API_URL + `recibo/${idCompra}`);
+        const blob = await this.ServicoHTTP.Request(Pedido_URL, "GET", undefined, true)
+        const ficheiro = new Blob([blob], { type: 'application/pdf' });
+        saveAs(ficheiro, `recibo_${idCompra}.pdf`); //LocalPartida
+    }
 }
 
